8cad8b896a445151dfab87a05786a3625814a85b
ramesh banakar <ramesh.s.b@campusuvce.in>
2026-05-31 14:33:52 +0530

    small prime factors

--- src/Combinations/SamllPrimeFactors.java
+++ src/Combinations/SamllPrimeFactors.java
@@ -1,14 +1,23 @@
 package Combinations;
 
-import java.util.Arrays;
-
 public class SamllPrimeFactors {
     public static void smallPrimeFactors(int n){
         int arr[]=new int[n+1];
         for(int i=0;i<n+1;i++){
             arr[i]=i;
         }
-        System.out.println(Arrays.toString(arr));
+
+        for(int i=2;i<=n;i++){
+            if(arr[i]==i){
+                for(int j=i*i;j<=n;j+=i){
+                    arr[j]=i;
+                }
+            }
+        }
+
+        for(int i=0;i<=n;i++){
+            System.out.print(arr[i]+" --> ");
+        }
     }
     public static void main(String[] args) {
         int n=40;
