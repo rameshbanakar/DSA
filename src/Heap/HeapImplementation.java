dd20a1c6809b593da9f08a2f507f2a5b2be5117b
ramesh banakar <ramesh.s.b@campusuvce.in>
2026-06-14 11:36:17 +0530

    downheapify implementations

--- src/Heap/HeapImplementation.java
+++ src/Heap/HeapImplementation.java
@@ -1,6 +1,7 @@
 package Heap;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class HeapImplementation {
     public static void upHepefy(ArrayList<Integer> heap, int value) {
@@ -12,10 +13,38 @@ public class HeapImplementation {
                 int temp = heap.get(i);
                 heap.set(i, heap.get(parent));
                 heap.set(parent, temp);
+                i = parent;
             } else {
                 break;
             }
         }
+    }
+
+    public static void downheapify(ArrayList<Integer> heap, int value) {
+        heap.add(value);
+
+        int i = 0;
+
+        while (2 * i + 1 < heap.size()) {
+            int child1 = heap.get(2 * i + 1);
+            int child2 = heap.get(2 * i + 2);
+            int min = IntStream.of(heap.get(i), child1, child2).min().getAsInt();
+            if (min == heap.get(i)) return;
+            else if (min == child1) {
+                int temp = heap.get(i);
+                heap.set(i, child1);
+                heap.set(2 * i + 1, temp);
+                i = 2 * i + 1;
+            } else if (min == child2) {
+                int temp = heap.get(i);
+                heap.set(i, child2);
+                heap.set(2 * i + 2, temp);
+                i = 2 * i + 2;
+            } else {
+                break;
+            }
+        }
+
     }
 
     public static void main(String[] args) {
@@ -25,6 +54,9 @@ public class HeapImplementation {
         upHepefy(heap, 4);
         System.out.println(heap);
         upHepefy(heap, 9);
+        System.out.println(heap);
+
+        downheapify(heap, 20);
         System.out.println(heap);
 
     }
