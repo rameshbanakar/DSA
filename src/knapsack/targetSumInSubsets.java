f3da4bcadb22d88fbf9690809e61dab6a358d3cc
ramesh banakar <ramesh.s.b@campusuvce.in>
2026-06-18 04:58:45 +0530

    target sum in the subset of the array

---
+++ src/knapsack/targetSumInSubsets.java
@@ -0,0 +1,24 @@
+package knapsack;
+
+public class targetSumInSubsets {
+    public static boolean targetSum(int[] arr, int index, int target) {
+        if (target == 0) return true;
+
+        if (index >= arr.length) return false;
+        boolean pick = false;
+        if (arr[index] <= target) {
+            pick = targetSum(arr, index + 1, target - arr[index]);
+        }
+        boolean unpick = targetSum(arr, index + 1, target);
+
+        return pick || unpick;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {3, 34, 4, 12, 5, 2};
+        int target = 9;
+        System.out.println(targetSum(a, 0, target));
+
+
+    }
+}
